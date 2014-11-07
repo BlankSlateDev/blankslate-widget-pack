c4af66c9b885e01ba427f22dae5f1d19c4c14da7
Dillon <dillon@blankslate.com>
2014-11-07 17:06:39 -0500

    update to 2.2.1

--- blankslate-widget-pack.php
+++ blankslate-widget-pack.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: BlankSlate Widget Pack
 Description: Collection of Widgets to Enhance BlankSlate Directory and Publishing Tools.
-Version: 2.2
+Version: 2.2.1
 Author: BlankSlate Factory, Inc.
 Author URI: http://blankslate.com
 Text Domain: blankslate
